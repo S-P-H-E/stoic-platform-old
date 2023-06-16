21224fcbdd2177235850c249a96a42778f15e4e3
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-06-16 19:18:53 +0200

    api webhook

---
+++ pages/api/keys.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+      res.status(200).json({
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      });
+    } else {
+      res.status(405).end('Method Not Allowed');
+    }
+  }
+  
---
+++ pages/api/webhooks.js
@@ -0,0 +1,55 @@
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const webhookHandler = async (req, res) => {
+  const buf = await new Promise((resolve, reject) => {
+    const chunks = [];
+    req
+      .on('data', (chunk) => chunks.push(chunk))
+      .on('end', () => resolve(Buffer.concat(chunks)))
+      .on('error', reject);
+  });
+
+  const payload = buf.toString();
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(payload, sig, process.env.STRIPE_SECRET_KEY);
+  } catch (err) {
+    console.log('Webhook signature verification failed.');
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the event
+switch (event.type) {
+  case 'payment_intent.succeeded':
+    // Handle payment_intent.succeeded event
+    console.log('Payment succeeded:', event.data.object);
+    break;
+  case 'payment_intent.payment_failed':
+    // Handle payment_intent.payment_failed event
+    console.log('Payment failed:', event.data.object);
+    break;
+  case 'invoice.created':
+    // Handle invoice.created event
+    console.log('Invoice created:', event.data.object);
+    break;
+  // Add other event handlers as needed
+  default:
+    console.log(`Unhandled event type: ${event.type}`);
+}
+
+  res.status(200).json({ received: true });
+};
+
+export default webhookHandler;
