4ec0f8facff78ba76a588d2b5e7db204d78f6678
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-07-08 15:50:32 +0200

    webhook needa fix

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,45 +1,33 @@
 import { buffer } from 'micro';
-import Stripe from 'stripe';
-
-// Retrieve the webhook signing secret from environmental variables
-const webhookSigningSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET;
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  // Set your preferred Stripe API version
-  apiVersion: '2022-11-15',
-});
+import Cors from 'micro-cors';
+import stripe from 'stripe';
 
 const webhookHandler = async (req, res) => {
   if (req.method === 'POST') {
-    const buf = await buffer(req);
-    const signature = req.headers['stripe-signature'];
-
-    let event;
-
     try {
-      // Verify the webhook signature
-      event = stripe.webhooks.constructEvent(
-        buf.toString(),
-        signature,
-        webhookSigningSecret
-      );
-    } catch (err) {
-      console.error('Webhook error:', err.message);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
+      const buf = await buffer(req);
+      const secret = process.env.STRIPE_WEBHOOK_SECRET; // Your webhook signing secret
 
-    // Handle the event based on its type
-    if (event.type === 'checkout.session.completed') {
-      // Handle checkout.session.completed event
-      console.log('Checkout session completed:', event.data.object);
-      // Perform any necessary actions or trigger desired functionality
-    }
+      // Verify the request signature
+      const signature = req.headers['stripe-signature'];
+      const event = stripe.webhooks.constructEvent(buf.toString(), signature, secret);
 
-    // Return a 200 response to acknowledge receipt of the event
-    res.status(200).send('Received');
+      // Handle the event
+      // Your logic to process the event goes here
+
+      res.status(200).send('Webhook received');
+    } catch (error) {
+      console.error('Error processing webhook:', error);
+      res.status(400).send('Webhook error');
+    }
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
 };
 
-export default webhookHandler;
+const cors = Cors({
+  allowMethods: ['POST'],
+});
+
+export default cors(webhookHandler);
